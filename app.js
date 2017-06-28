d191b7531463e944019055ee5a2e4d2fcea774f4
Rohit Singh <rohitsingh17219@gmail.com>
2017-06-28 23:11:26 +0530

    week2_lecture13 create custom filter - palindrom filter

--- app.js
+++ app.js
@@ -3,10 +3,22 @@
 
   angular
     .module('MyFirstModule', [])
-    .controller('MyFirstController', MyController);
+    .controller('MyFirstController', MyController)
+    .filter('palindrom', PalindromFilterFactory);
 
-  MyController.$inject = ['$scope'];
+  MyController.$inject = ['$scope', 'palindromFilter'];
 
-  function MyController($scope) {
+  function MyController($scope, palindromFilter) {
+    $scope.name = "Ro";
+
+    $scope.reverseMe = function() {
+      return palindromFilter($scope.name);
+    };
+  };
+
+  function PalindromFilterFactory() {
+    return function(input) {
+      return input.split('').reverse().join('');
+    }
   };
 })();
