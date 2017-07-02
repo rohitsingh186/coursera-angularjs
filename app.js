5d23f76f0305f6c32211048e777a1b408ad22640
Rohit Singh <rohitsingh17219@gmail.com>
2017-07-02 14:59:41 +0530

    Standaried week_2

--- app.js
+++ app.js
@@ -21,11 +21,6 @@
   MyController.$inject = ['$scope', '$filter', 'palindromFilter'];
 
   function MyController($scope, $filter, palindromFilter) {
-    // week_1
-    $scope.name = 'POOJA SHARMA';
-    $scope.asciiSum = 853;
-    $scope.flashIsRunning = true;
-
     // week_2
     $scope.myName = "Rohit Singh";
     $scope.shoppingList = [
@@ -50,37 +45,6 @@
     }
     $scope.child = Object.create($scope.parent);
     $scope.child.name = 'Child';
-
-    // week_1
-    $scope.sayHello = function() {
-      return 'Hello';
-    };
-
-    $scope.uppercaseName = function() {
-      var upcase = $filter('uppercase');
-      $scope.name = upcase($scope.name);
-    };
-
-    $scope.updateAscii = function() {
-      var tempAsciiSum = 0;
-      for (var i = 0; i < $scope.name.length; i++) {
-        tempAsciiSum += $scope.name.charCodeAt(i);
-      }
-      $scope.asciiSum = tempAsciiSum;
-      return $scope.asciiSum;
-    };
-
-    $scope.flashStatus = function() {
-      return ($scope.flashIsRunning ? 'running' : 'standing');
-    };
-
-    $scope.flashImgExt = function() {
-      return ($scope.flashIsRunning ? 'gif' : 'jpg');
-    };
-
-    $scope.toggleFlashStatus = function() {
-      $scope.flashIsRunning = $scope.flashIsRunning ? false : true;
-    };
 
     // week_2
     $scope.reverseMe = function() {
